a1a82bc81f4a39f7ee98426280f392f4a4c258e1
Troy Prelog <35702532+tprelog@users.noreply.github.com>
2020-08-11 02:51:34 -0400

    config flow p1

--- custom_components/truenas/__init__.py
+++ custom_components/truenas/__init__.py
@@ -1,15 +1,72 @@
 """Example Load Platform integration."""
-DOMAIN = 'example_load_platform'
+import asyncio
+import logging
+
+import homeassistant.helpers.config_validation as cv
+
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from homeassistant.const import (
+    CONF_API_KEY,
+    CONF_HOST,
+    )
+
+from .const import DOMAIN
+
+CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_API_KEY): cv.string,
+    })}, extra=vol.ALLOW_EXTRA)
+
+PLATFORMS = ["sensor"]
 
 
-def setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: dict):
     """Your controller/hub specific code."""
     # Data that you want to share with your platforms
     hass.data[DOMAIN] = {
-        'state': "OK",
-        'temperature': 32
+        'state': "DAZED",
+        'temperature': 5
+    }
+    
+    for component in PLATFORMS:
+        hass.helpers.discovery.load_platform(component, DOMAIN, {}, config)
+    
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up A Default Integration from a config entry."""
+    # TODO Store an API object for your platforms to access
+    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+
+    hass.data[DOMAIN] = {
+        'state': "CONFUSED",
+        'temperature': 15
     }
 
-    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
 
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
--- custom_components/truenas/sensor.py
+++ custom_components/truenas/sensor.py
@@ -1,44 +1,39 @@
 """Platform for sensor integration."""
+import asyncio
+import logging
+
 from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
 
-from . import DOMAIN
+from .const import (
+    DOMAIN,
+    STATE_ATTR_STATUS,
+    STATE_ATTR_VERSION,
+)
+
+_LOGGER = logging.getLogger(__name__)
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
     # We only want this platform to be set up via discovery.
     if discovery_info is None:
         return
-    add_entities([ExampleSensor1(),ExampleSensor2()])
+        
+    arg_n = "arg_n value"
+    add_entities([ExampleSensor1(),TrueNASSensor(config, arg_n, "Blah")])
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Add a weather entity from a config_entry."""
+    # NOTE example: async_add_entities([MetWeather(config_entry.data, hass.config.units.is_metric)])
+    
+    arg_n = "arg_n value"
+    #async_add_entities([TrueNASMusicSensor(config_entry, arg_n, "Blah")]) # maybe Hint hass.config.somethnig??
+    async_add_entities([TrueNASSensor(config_entry, arg_n, "Blah")]) # maybe Hint hass.config.somethnig??
 
 
 class ExampleSensor1(Entity):
-    """Representation of a sensor."""
-
-    def __init__(self):
-        """Initialize the sensor."""
-        self._state = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return 'Example Platform Sensor 1'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._state = self.hass.data[DOMAIN]['state']
-
-
-class ExampleSensor2(Entity):
     """Representation of a sensor."""
 
     def __init__(self):
@@ -66,3 +61,72 @@ class ExampleSensor2(Entity):
         This is the only method that should fetch new data for Home Assistant.
         """
         self._state = self.hass.data[DOMAIN]['temperature']
+
+
+class TrueNASSensor(Entity):
+    # NOTE: https://github.com/home-assistant/core/blob/dev/homeassistant/components/plaato/sensor.py#L69
+    """Representation of a sensor."""
+    def __init__(self, arg1_config, arg_n, arg_x):
+        """Initialize the sensor."""
+        
+        self._config = arg1_config        
+        self._static = arg_x
+        self._arg_n = arg_n
+        
+        self._state = None
+        self._connected = False
+        self._cpu_temp = 0
+
+        self._name = "Fucker"        
+        #self._name = arg1_config.host
+        
+        # TODO: Keep learning
+        # -ONE-:  self._devide_id = arg1_config["device_id"]
+        # -TWO-:  self._devide_id = self._config["device_id"]
+        # Try to use the variable value in a property will error -- CONF_DEVICE_ID is not defined
+        # - See @property "state_attributes" -- NOTE: Set at top of file instead
+        #CONF_DEVICE_ID = "device_id"
+        #self._devide_id = arg1_config.data[CONF_DEVICE_ID]
+        
+        #self._config = arg1_config.data > Results is error > unable to serialize JSON: type mapping error
+        #self._devide_id = arg1_config.options[CONF_DEVICE_ID]
+        
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+    
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._cpu_temp
+          
+    @property
+    def state_attributes(self):
+        """Return the state attributes of the sun."""
+        return {
+            "Arg N": self._arg_n,
+            "Static (arg_x)": self._static,
+            "Value": "Static value",
+            
+            "CPU Temp": self._cpu_temp,
+            "STATE_ATTR_STATUS": self._connected,
+            
+            ## NOTE: After moving CONF_DEVICE_ID to top this works
+            #CONF_DEVICE_ID: self._devide_id,
+            #- "device_id": self._devide_id,
+            
+            ## NOTE: Helper attr to view config_entry
+            "config": self._config
+        }
+        
+        
+    # https://developers.home-assistant.io/docs/entity_index#updating-the-entity
+    # This is the only method that should fetch new data for Home Assistant.
+    async def async_update(self):
+        """ Fetch new state data for the sensor. """
+        # TODO: Something relevant here... sync library or something
+        #self._state = self.hass.data[GM_DOMAIN]['state']
+        #self._connected = self.hass.data[DOMAIN]['connected']
+        self._connected = self.hass.data[DOMAIN]['state'],
+        self._cpu_temp = self.hass.data[DOMAIN]['temperature']
